82bbc4d36a77b627a5bbb6c0578fe91229aa1b50
MarkMizzi <mizzimark2001@gmail.com>
2023-03-25 21:45:21 +0100

    started parser implementation

--- src/parser.cc
+++ src/parser.cc
@@ -1,3 +1,67 @@
 #include "parser.hh"
+#include "ast.hh"
+#include "lexer.hh"
 
-namespace parser {}
+#include <stdexcept>
+
+namespace parser {
+
+BinaryExprNode::BinaryOp tokenTypeToBinaryOp(lexer::TokenType tokType) {
+  switch (tokType) {
+  case lexer::PLUS_TOK:
+    return BinaryExprNode::BinaryOp::ADD;
+  case lexer::MINUS_TOK:
+    return BinaryExprNode::BinaryOp::SUB;
+  case lexer::STAR_TOK:
+    return BinaryExprNode::BinaryOp::MUL;
+  case lexer::DIV_TOK:
+    return BinaryExprNode::BinaryOp::DIV;
+  case lexer::AND:
+    return BinaryExprNode::BinaryOp::AND;
+  case lexer::OR:
+    return BinaryExprNode::BinaryOp::OR;
+  case lexer::GREATER_TOK:
+    return BinaryExprNode::BinaryOp::GREATER;
+  case lexer::LESS_TOK:
+    return BinaryExprNode::BinaryOp::LESS;
+  case lexer::EQ_TOK:
+    return BinaryExprNode::BinaryOp::EQ;
+  case lexer::NEQ_TOK:
+    return BinaryExprNode::BinaryOp::NEQ;
+  case lexer::GE:
+    return BinaryExprNode::BinaryOp::GE;
+  case lexer::LE:
+    return BinaryExprNode::BinaryOp::LE;
+  default:
+    throw std::logic_error("Tokens of type " + std::to_string(tokType) +
+                           " do not represent a binary operator.");
+  }
+}
+
+ExprNodePtr Parser::parseFactor() {}
+
+ExprNodePtr Parser::parseTerm() {}
+
+ExprNodePtr Parser::parseSimpleExpr() {}
+
+ExprNodePtr Parser::parseExpr() {
+  ExprNodePtr left = parseSimpleExpr();
+  switch (peek(0).type) {
+  case lexer::GREATER_TOK:
+  case lexer::LESS_TOK:
+  case lexer::EQ_TOK:
+  case lexer::NEQ_TOK:
+  case lexer::GE:
+  case lexer::LE: {
+    BinaryExprNode::BinaryOp op = tokenTypeToBinaryOp(consume().type);
+    ExprNodePtr right = parseSimpleExpr();
+
+    return std::make_unique<BinaryExprNode>(op, std::move(left),
+                                            std::move(right), Location{});
+  }
+  default:
+    return left;
+  }
+}
+
+} // namespace parser
--- src/parser.hh
+++ src/parser.hh
@@ -1,6 +1,44 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-namespace parser {}
+#include "ast.hh"
+#include "lexer.hh"
+
+#include <deque>
+
+namespace parser {
+
+class Parser {
+private:
+  lexer::Lexer *lexer;
+  std::deque<lexer::Token> lookahead;
+
+  lexer::Token peek(size_t i) {
+    if (i > lookahead.size()) {
+      lexer::Token tok = lexer->GetNextToken();
+      lookahead.push_back(tok);
+      return tok;
+    }
+    return lookahead[i];
+  }
+
+  lexer::Token consume() {
+    if (lookahead.size()) {
+      lexer::Token tok = lookahead.front();
+      lookahead.pop_front();
+      return tok;
+    }
+    return lexer->GetNextToken();
+  }
+
+public:
+  Parser(lexer::Lexer *lexer) : lexer(lexer) {}
+
+  ExprNodePtr parseFactor();
+  ExprNodePtr parseTerm();
+  ExprNodePtr parseSimpleExpr();
+  ExprNodePtr parseExpr();
+};
+} // namespace parser
 
 #endif // PARSER_H_
