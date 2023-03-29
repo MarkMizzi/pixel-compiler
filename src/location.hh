73a790720cbccefbc39444d0f9b57e0842d31ab9
MarkMizzi <mizzimark2001@gmail.com>
2023-03-29 21:00:34 +0200

    Implemented Location::to_string

--- src/location.hh
+++ src/location.hh
@@ -2,6 +2,7 @@
 #define LOCATION_H_
 
 #include <algorithm>
+#include <string>
 
 struct Location {
   size_t sline, scol, eline, ecol;
@@ -10,6 +11,11 @@ struct Location {
     return Location{
         std::min(this->sline, other.sline), std::min(this->scol, other.scol),
         std::max(this->eline, other.eline), std::max(this->ecol, other.ecol)};
+  }
+
+  std::string to_string() {
+    return std::to_string(sline) + ":" + std::to_string(scol) + ":" +
+           std::to_string(eline) + ":" + std::to_string(ecol);
   }
 };
 
