bc9bb4b087313c6421f0a3eea579e575ced85794
MarkMizzi <mizzimark2001@gmail.com>
2023-03-29 22:02:42 +0200

    added main that prints the input

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,14 @@
-int main() {}
+#include "xml_printer.hh"
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+  if (argc <= 1) {
+    std::cerr << "Expected filename." << std::endl;
+    exit(-1);
+  }
+
+  XMLPrinter xmlp{argv[1]};
+  std::cout << xmlp.xml() << std::endl;
+}
