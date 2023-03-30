32281dbd40476b16952952591c6ab635e830fb2e
MarkMizzi <mizzimark2001@gmail.com>
2023-03-30 06:39:48 +0200

    added two ad-hoc lexer tests

--- tests/lexer_tests.cc
+++ tests/lexer_tests.cc
@@ -2,6 +2,7 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <algorithm>
 #include <sstream>
 #include <string>
 
@@ -88,4 +89,138 @@ TEST_CASE("Multi-line comments are filtered out", "[lexer]") {
   ss.seekp(0);
   lexer::Lexer lexer{ss};
   REQUIRE(lexer.getNextToken().type == lexer::TokenType::END);
+}
+
+TEST_CASE("Ad-hoc lexer test 1", "[lexer]") {
+  std::stringstream ss{"fun AverageOfTwo(x: int, y : int) -> float {\n"
+                       "    let t0 : int = x + y;\n"
+                       "    let t1 : float = t0 / 2 ;\n"
+                       "    return t1 ;\n"
+                       "}"};
+  ss.seekp(0);
+  lexer::Lexer lexer{ss};
+
+  std::vector<lexer::Token> toks;
+  lexer::Token tok;
+  while ((tok = lexer.getNextToken()).type != lexer::END) {
+    toks.push_back(tok);
+  }
+
+  std::vector<lexer::TokenType> tokTypes(toks.size());
+  std::vector<std::string> tokValues(toks.size());
+
+  std::transform(toks.begin(), toks.end(), tokTypes.begin(),
+                 [](lexer::Token &tok) { return tok.type; });
+  std::transform(toks.begin(), toks.end(), tokValues.begin(),
+                 [](lexer::Token &tok) { return tok.value; });
+
+  REQUIRE(tokTypes == std::vector<lexer::TokenType>{lexer::FUN,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::LBRACKET_TOK,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::COLON_TOK,
+                                                    lexer::INT,
+                                                    lexer::COMMA_TOK,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::COLON_TOK,
+                                                    lexer::INT,
+                                                    lexer::RBRACKET_TOK,
+                                                    lexer::ARROW,
+                                                    lexer::FLOAT,
+                                                    lexer::LBRACE_TOK,
+                                                    lexer::LET,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::COLON_TOK,
+                                                    lexer::INT,
+                                                    lexer::ASSIGN,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::PLUS_TOK,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::SEMICOLON_TOK,
+                                                    lexer::LET,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::COLON_TOK,
+                                                    lexer::FLOAT,
+                                                    lexer::ASSIGN,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::DIV_TOK,
+                                                    lexer::INTEGER_LITERAL,
+                                                    lexer::SEMICOLON_TOK,
+                                                    lexer::RETURN,
+                                                    lexer::IDENTIFIER,
+                                                    lexer::SEMICOLON_TOK,
+                                                    lexer::RBRACE_TOK});
+
+  REQUIRE(tokValues == std::vector<std::string>{"fun",    "AverageOfTwo",
+                                                "(",      "x",
+                                                ":",      "int",
+                                                ",",      "y",
+                                                ":",      "int",
+                                                ")",      "->",
+                                                "float",  "{",
+                                                "let",    "t0",
+                                                ":",      "int",
+                                                "=",      "x",
+                                                "+",      "y",
+                                                ";",      "let",
+                                                "t1",     ":",
+                                                "float",  "=",
+                                                "t0",     "/",
+                                                "2",      ";",
+                                                "return", "t1",
+                                                ";",      "}"});
+}
+
+TEST_CASE("Ad-hoc lexer test 2", "[lexer]") {
+  std::stringstream ss{
+      "/* Same functionality as function above but using less code.\n"
+      " * Note the use of brackets in the expression following the\n"
+      " * return statement. Allocates space for 2 variables.\n"
+      " */\n"
+      "fun AverageOfTwo2( x : int, y : int ) -> float {"
+      "return (x + y) / 2;"
+      "}"};
+  ss.seekp(0);
+  lexer::Lexer lexer{ss};
+
+  std::vector<lexer::Token> toks;
+  lexer::Token tok;
+  while ((tok = lexer.getNextToken()).type != lexer::END) {
+    toks.push_back(tok);
+  }
+
+  std::vector<lexer::TokenType> tokTypes(toks.size());
+  std::vector<std::string> tokValues(toks.size());
+
+  std::transform(toks.begin(), toks.end(), tokTypes.begin(),
+                 [](lexer::Token &tok) { return tok.type; });
+  std::transform(toks.begin(), toks.end(), tokValues.begin(),
+                 [](lexer::Token &tok) { return tok.value; });
+
+  REQUIRE(tokTypes == std::vector<lexer::TokenType>{
+                          lexer::FUN,           lexer::IDENTIFIER,
+                          lexer::LBRACKET_TOK,  lexer::IDENTIFIER,
+                          lexer::COLON_TOK,     lexer::INT,
+                          lexer::COMMA_TOK,     lexer::IDENTIFIER,
+                          lexer::COLON_TOK,     lexer::INT,
+                          lexer::RBRACKET_TOK,  lexer::ARROW,
+                          lexer::FLOAT,         lexer::LBRACE_TOK,
+                          lexer::RETURN,        lexer::LBRACKET_TOK,
+                          lexer::IDENTIFIER,    lexer::PLUS_TOK,
+                          lexer::IDENTIFIER,    lexer::RBRACKET_TOK,
+                          lexer::DIV_TOK,       lexer::INTEGER_LITERAL,
+                          lexer::SEMICOLON_TOK, lexer::RBRACE_TOK});
+
+  REQUIRE(tokValues == std::vector<std::string>{"fun",    "AverageOfTwo2",
+                                                "(",      "x",
+                                                ":",      "int",
+                                                ",",      "y",
+                                                ":",      "int",
+                                                ")",      "->",
+                                                "float",  "{",
+                                                "return", "(",
+                                                "x",      "+",
+                                                "y",      ")",
+                                                "/",      "2",
+                                                ";",      "}"});
 }
