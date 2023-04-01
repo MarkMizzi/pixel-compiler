c4a018ef4b78b4167eab9d381d3975a5705a8aa8
MarkMizzi <mizzimark2001@gmail.com>
2023-04-01 12:13:18 +0200

    commit util file with CompilationError

---
+++ src/util.hh
@@ -0,0 +1,11 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <stdexcept>
+
+class CompilationError : public std::runtime_error {
+public:
+  using std::runtime_error::runtime_error;
+};
+
+#endif // UTIL_H_
