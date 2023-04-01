eb6410f46767a34066c267ee89defe9a1ab619da
MarkMizzi <mizzimark2001@gmail.com>
2023-04-01 09:23:23 +0200

    made Compiler class more versatile

--- src/compiler.hh
+++ src/compiler.hh
@@ -6,32 +6,47 @@
 #include "lexer.hh"
 #include "parser.hh"
 #include "semantic_visitor.hh"
+#include "xml_visitor.hh"
 
-#include <fstream>
+#include <iostream>
 #include <memory>
 #include <string>
 
+struct CompilerOptions {
+  bool generateXml;
+  std::ostream &xmlStream = std::cout;
+};
+
 class Compiler {
 private:
-  std::fstream fs;
+  std::ostream &out;
   lexer::Lexer lexer;
   parser::Parser parser;
+  ast::XMLVisitor xmlVisitor;
   ast::SymbolTable symbolTable;
   ast::SemanticVisitor semanticChecker;
   codegen::CodeGenerator codeGenerator;
 
-public:
-  Compiler(std::string fname)
-      : fs(fname, fs.in), lexer(lexer::Lexer(fs)), parser(lexer),
-        semanticChecker(symbolTable), codeGenerator(symbolTable) {}
+  CompilerOptions opts;
 
-  const codegen::PixIRCode compile() {
+public:
+  Compiler(std::istream &in, std::ostream &out, CompilerOptions &opts)
+      : out(out), lexer(lexer::Lexer(in)), parser(lexer),
+        semanticChecker(symbolTable), codeGenerator(symbolTable), opts(opts) {}
+
+  void compile() {
     std::unique_ptr<ast::TranslationUnit> tu{parser.parse()};
     semanticChecker.visit(*tu);
+
+    if (opts.generateXml) {
+      xmlVisitor.visit(*tu);
+      opts.xmlStream << xmlVisitor.xml();
+    }
+
     codeGenerator.visit(*tu);
     codegen::PixIRCode &code(codeGenerator.code());
     codegen::linearizeCode(code);
-    return std::move(code);
+    codegen::dumpCode(code, out);
   }
 };
 
