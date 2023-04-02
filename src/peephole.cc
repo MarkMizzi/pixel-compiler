31de008c7b3959336a6e82b3d07a1e3229780145
MarkMizzi <mizzimark2001@gmail.com>
2023-04-02 18:15:23 +0200

    added more rules for the peephole optimizer

--- src/peephole.cc
+++ src/peephole.cc
@@ -5,18 +5,49 @@ namespace codegen {
 
 static const std::vector<std::pair<PixIRPattern, CodePeephole>> patterns{
     {PixIRPattern({{PixIROpcode::PUSH, "0"}, {PixIROpcode::ALLOC}}), {}},
+
     {PixIRPattern(
          {{PixIROpcode::GT}, {PixIROpcode::PUSH, "1"}, {PixIROpcode::SUB}}),
      {{PixIROpcode::LE}}},
+
     {PixIRPattern(
          {{PixIROpcode::LT}, {PixIROpcode::PUSH, "1"}, {PixIROpcode::SUB}}),
      {{PixIROpcode::GE}}},
+
     {PixIRPattern(
          {{PixIROpcode::GE}, {PixIROpcode::PUSH, "1"}, {PixIROpcode::SUB}}),
      {{PixIROpcode::LT}}},
+
     {PixIRPattern(
          {{PixIROpcode::LE}, {PixIROpcode::PUSH, "1"}, {PixIROpcode::SUB}}),
      {{PixIROpcode::GT}}},
+
+    {PixIRPattern(
+         {{PixIROpcode::GT}, {PixIROpcode::PUSH, "0"}, {PixIROpcode::EQ}}),
+     {{PixIROpcode::LE}}},
+
+    {PixIRPattern(
+         {{PixIROpcode::LT}, {PixIROpcode::PUSH, "0"}, {PixIROpcode::EQ}}),
+     {{PixIROpcode::GE}}},
+
+    {PixIRPattern(
+         {{PixIROpcode::GE}, {PixIROpcode::PUSH, "0"}, {PixIROpcode::EQ}}),
+     {{PixIROpcode::LT}}},
+
+    {PixIRPattern(
+         {{PixIROpcode::LE}, {PixIROpcode::PUSH, "0"}, {PixIROpcode::EQ}}),
+     {{PixIROpcode::GT}}},
+
+    {PixIRPattern({{PixIROpcode::PUSH, "1"}, {PixIROpcode::ADD}}),
+     {{PixIROpcode::INC}}},
+
+    {PixIRPattern({{PixIROpcode::PUSH, "0"}, {PixIROpcode::ADD}}), {}},
+
+    {PixIRPattern({{PixIROpcode::PUSH, "0"}, {PixIROpcode::IRND}}),
+     {{PixIROpcode::PUSH, "0"}}},
+
+    {PixIRPattern({{PixIROpcode::PUSH, "0"}, {PixIROpcode::DELAY}}), {{}}},
+
 };
 
 void peepholeOptimize(PixIRCode &code) {
