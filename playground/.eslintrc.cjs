b22d2ea057d63bf5eaf5011fea153f2ddd107cf1
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 05:14:21 +0200

    format eslint file

--- playground/.eslintrc.cjs
+++ playground/.eslintrc.cjs
@@ -3,9 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
