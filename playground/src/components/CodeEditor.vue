eb8187a9fe86b141bd01494d5d04876fb4181e24
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 04:57:24 +0200

    add CodeEditor component

---
+++ playground/src/components/CodeEditor.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CodeMirror from 'codemirror'
+import { useTemplateRef } from 'vue'
+
+const props = defineProps<{
+  readonly: boolean
+  mode: string
+}>()
+
+const editorTextAreaRef = useTemplateRef('editor')
+CodeMirror.fromTextArea(editorTextAreaRef.value as HTMLTextAreaElement, {
+  value: '',
+  mode: props.mode
+})
+</script>
+
+<template>
+  <textarea ref="editor" :readonly="readonly"> </textarea>
+</template>
+
+<style scoped></style>
