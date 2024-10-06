918f381cfc22ed87656fa9541e6999629a4d6b18
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 06:59:25 +0200

    added vue-toast-notification

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ToastPlugin from 'vue-toast-notification'
+// Import one of the available themes
+import 'vue-toast-notification/dist/theme-default.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ToastPlugin)
 app.use(router)
 
 app.mount('#app')
