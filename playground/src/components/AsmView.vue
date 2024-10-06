fae8e9112e83ab459bf95f3bea2bc20add582242
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 07:46:09 +0200

    add views for AST and assembly

---
+++ playground/src/components/AsmView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CodeEditor from './CodeEditor.vue'
+import { useAssemblyStore } from '@/stores/asm'
+
+const asmStore = useAssemblyStore()
+</script>
+
+<template>
+  <CodeEditor
+    ref="astXmlCodeEditor"
+    class="border-2 border-slate-700"
+    readonly="nocursor"
+    mode="null"
+    :content="asmStore.asm"
+  ></CodeEditor>
+</template>
+
+<style scoped></style>
---
+++ playground/src/components/AstXmlView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CodeEditor from './CodeEditor.vue'
+import { useAstXmlStore } from '@/stores/ast'
+
+const astXmlStore = useAstXmlStore()
+</script>
+
+<template>
+  <CodeEditor
+    ref="astXmlCodeEditor"
+    class="border-2 border-slate-700"
+    readonly="nocursor"
+    mode="xml"
+    :content="astXmlStore.astXml"
+  ></CodeEditor>
+</template>
+
+<style scoped></style>
