9d8e65bd9d2cf9d42d94baacddc1bc97aa9e3dc0
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 14:25:27 +0200

    implement sketch of HomeView which connects the various components

--- playground/src/views/HomeView.vue
+++ playground/src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import AsmView from '@/components/AsmView.vue'
+import AstXmlView from '@/components/AstXmlView.vue'
+import SourceCodeView from '@/components/SourceCodeView.vue'
+import { defineComponent, useTemplateRef } from 'vue'
+
+export default defineComponent({
+  methods: {
+    setAstXml(astXml: string) {
+      const astXmlView = useTemplateRef('astXmlView')
+      ;(astXmlView.value as typeof AstXmlView).setAstXml(astXml)
+    },
+    setAssembly(assembly: string) {
+      const asmView = useTemplateRef('asmView')
+      ;(asmView.value as typeof AsmView).setAssembly(assembly)
+    }
+  }
+})
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="flex flex-col lg:flex-row">
+      <SourceCodeView :set-assembly="setAssembly" , :set-ast-xml="setAstXml"></SourceCodeView>
+      <AsmView ref="asmView"></AsmView>
+      <div class="hidden">
+        <AstXmlView ref="astXmlView"></AstXmlView>
+      </div>
+    </div>
+  </main>
 </template>
