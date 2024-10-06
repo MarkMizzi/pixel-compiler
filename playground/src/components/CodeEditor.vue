da4f2c2e864e5add49cfa03b66127fe3b547bb13
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 05:14:38 +0200

    fix readonly property

--- playground/src/components/CodeEditor.vue
+++ playground/src/components/CodeEditor.vue
@@ -3,19 +3,20 @@ import CodeMirror from 'codemirror'
 import { useTemplateRef } from 'vue'
 
 const props = defineProps<{
-  readonly: boolean
+  readonly: boolean | 'nocursor'
   mode: string
 }>()
 
 const editorTextAreaRef = useTemplateRef('editor')
 CodeMirror.fromTextArea(editorTextAreaRef.value as HTMLTextAreaElement, {
   value: '',
-  mode: props.mode
+  mode: props.mode,
+  readOnly: props.readonly
 })
 </script>
 
 <template>
-  <textarea ref="editor" :readonly="readonly"> </textarea>
+  <textarea ref="editor"></textarea>
 </template>
 
 <style scoped></style>
