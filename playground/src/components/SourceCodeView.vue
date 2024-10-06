b479484371aea93e3d74fa611342b6b90efe4613
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 05:14:55 +0200

    sketch of SourceCodeView

---
+++ playground/src/components/SourceCodeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import CodeEditor from './CodeEditor.vue'
+
+function compile() {}
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-row justify-start">
+      <button class="w-32 p-2 rounded bg-teal-700 text-white" onclick="compile()">Compile</button>
+    </div>
+    <CodeEditor class="border-2 border-slate-700" :readonly="false" mode="null"></CodeEditor>
+  </div>
+</template>
+
+<style scoped></style>
