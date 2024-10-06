a5e20db5974c08ae8770a0dc2ac689bda606a10a
MarkMizzi <mizzimark2001@gmail.com>
2024-10-06 14:11:01 +0200

    update AST and Asm views

--- playground/src/components/AsmView.vue
+++ playground/src/components/AsmView.vue
@@ -1,10 +1,20 @@
-<script setup lang="ts">
+<script lang="ts">
 import CodeEditor from './CodeEditor.vue'
+import { defineComponent, useTemplateRef } from 'vue'
+
+export default defineComponent({
+  methods: {
+    setAssembly(assembly: string) {
+      const assemblyEditor = useTemplateRef('assemblyEditor')
+      ;(assemblyEditor.value as typeof CodeEditor).setCode(assembly)
+    }
+  }
+})
 </script>
 
 <template>
   <CodeEditor
-    ref="astXmlCodeEditor"
+    ref="assemblyEditor"
     class="border-2 border-slate-700"
     readonly="nocursor"
     mode="null"
--- playground/src/components/AstXmlView.vue
+++ playground/src/components/AstXmlView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts">
+<script lang="ts">
 import CodeEditor from './CodeEditor.vue'
+import { defineComponent, useTemplateRef } from 'vue'
+import { xml } from '@codemirror/lang-xml'
+
+export default defineComponent({
+  mounted() {
+    // add XML extension to the code editor.
+    const astXmlCodeEditor = useTemplateRef('astXmlCodeEditor')
+    ;(astXmlCodeEditor.value as typeof CodeEditor).addExtension(xml)
+  },
+  methods: {
+    setAstXml(astXml: string) {
+      const astXmlCodeEditor = useTemplateRef('astXmlCodeEditor')
+      ;(astXmlCodeEditor.value as typeof CodeEditor).setCode(astXml)
+    }
+  }
+})
 </script>
 
 <template>
