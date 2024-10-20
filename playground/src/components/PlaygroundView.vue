184079dfc595fa6bbaf78a868749b8ad4eeb0fd9
MarkMizzi <mizzimark2001@gmail.com>
2024-10-20 17:34:27 +0200

    refactor,move compile() to PlaygroundView

--- playground/src/components/PlaygroundView.vue
+++ playground/src/components/PlaygroundView.vue
@@ -1,41 +1,8 @@
-<script lang="ts">
-import AsmView from '@/components/AsmView.vue'
-import AstXmlView from '@/components/AstXmlView.vue'
-import PixelVMView from '@/components/PixelVMView.vue'
-import SourceCodeView from '@/components/SourceCodeView.vue'
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  components: {
-    SourceCodeView,
-    AstXmlView,
-    AsmView,
-    PixelVMView
-  },
-  data() {
-    return { selected: 'assembly' }
-  },
-  methods: {
-    setAstXml(astXml: string) {
-      const astXmlView = this.$refs.astXmlView
-      ;(astXmlView as typeof AstXmlView).setContent(astXml)
-    },
-    setAssembly(assembly: string) {
-      const asmView = this.$refs.asmView
-      ;(asmView as typeof AsmView).setContent(assembly)
-
-      const pixelVMView = this.$refs.pixelVMView
-      ;(pixelVMView as typeof PixelVMView).setProgram(assembly)
-    }
-  }
-})
-</script>
-
 <template>
   <main>
     <div class="flex flex-flow-row">
       <div class="w-full">
-        <SourceCodeView :set-assembly="setAssembly" :set-ast-xml="setAstXml"></SourceCodeView>
+        <SourceCodeView :compile="compile"></SourceCodeView>
       </div>
       <div class="w-full grid grid-cols-1">
         <div class="flex flex-row justify-start p-2 gap-x-4">
@@ -62,3 +29,79 @@ export default defineComponent({
     </div>
   </main>
 </template>
+
+<script lang="ts">
+import AsmView from '@/components/AsmView.vue'
+import AstXmlView from '@/components/AstXmlView.vue'
+import PixelVMView from '@/components/PixelVMView.vue'
+import SourceCodeView from '@/components/SourceCodeView.vue'
+import { StatusCodes } from 'http-status-codes'
+import { defineComponent } from 'vue'
+import { useToast } from 'vue-toast-notification'
+
+const $toast = useToast()
+
+interface CompilerOutput {
+  xmlOutput: string
+  asmOutput: string
+  compilerStdOut: string
+  compilerStdErr: string
+}
+
+export default defineComponent({
+  components: {
+    SourceCodeView,
+    AstXmlView,
+    AsmView,
+    PixelVMView
+  },
+  data() {
+    return { selected: 'assembly' }
+  },
+  methods: {
+    compile(srcCode: string) {
+      fetch('/compile', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          srcCode: srcCode
+        })
+      })
+        .then(async (response) => {
+          // check error status
+          if (response.status == StatusCodes.SERVICE_UNAVAILABLE) {
+            // this error comes from NGINX, give our own error message
+            throw Error('Rate limit exceeded. Try again later.')
+          } else if (response.status != 200) {
+            throw Error(`${response.status} ${await response.text()}`)
+          } else {
+            return response.json()
+          }
+        })
+        .then((data) => {
+          const astXmlView = this.$refs.astXmlView as typeof AstXmlView
+          const asmView = this.$refs.asmView as typeof AsmView
+          const pixelVMView = this.$refs.pixelVMView as typeof PixelVMView
+
+          // parse body
+          let output = data as CompilerOutput
+          // check for compiler error
+          if (output.asmOutput == '') {
+            astXmlView.setContent(output.compilerStdErr + output.compilerStdOut)
+            asmView.setContent(output.compilerStdErr + output.compilerStdOut)
+            // an error occurred, indicate to user and set asm and ast views to combined stderr and stdout streams.
+            throw Error('Compilation error occurred. Check your source program.')
+          } else {
+            // set content of various views accordingly
+            astXmlView.setContent(output.xmlOutput)
+            asmView.setContent(output.asmOutput)
+            pixelVMView.setProgram(output.asmOutput)
+          }
+        })
+        .catch((error) => {
+          $toast.error(`${error}`)
+        })
+    }
+  }
+})
+</script>
--- playground/src/components/SourceCodeView.vue
+++ playground/src/components/SourceCodeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid grid-cols-1">
     <div class="flex flex-row justify-start p-2 gap-x-2">
-      <button class="w-32 p-2 rounded bg-teal-700 text-white" @click="compile()">Compile</button>
+      <button class="w-32 p-2 rounded bg-teal-700 text-white" @click="onPressCompile()">
+        Compile
+      </button>
       <select
         id="select-example-src-code"
         class="bg-slate-700 text-white"
@@ -24,67 +26,21 @@
     ></CodeEditor>
   </div>
 </template>
+
 <script setup lang="ts">
-import { StatusCodes } from 'http-status-codes'
 import CodeEditor from './CodeEditor.vue'
 import { ref, type Component, type Ref } from 'vue'
-import { useToast } from 'vue-toast-notification'
-
-const $toast = useToast()
 
 const props = defineProps<{
-  setAstXml: (s: string) => void
-  setAssembly: (s: string) => void
+  compile: (s: string) => void
 }>()
 
 // once the component is mounted this will automatically be set to
 // point to child component with ref="srcCodeEditor"
 const srcCodeEditor: Ref<Component | null> = ref(null)
 
-interface CompilerOutput {
-  xmlOutput: string
-  asmOutput: string
-  compilerStdOut: string
-  compilerStdErr: string
-}
-
-const compile = () => {
-  fetch('/compile', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      srcCode: (srcCodeEditor.value as typeof CodeEditor).getCode()
-    })
-  })
-    .then(async (response) => {
-      // check error status
-      if (response.status == StatusCodes.SERVICE_UNAVAILABLE) {
-        // this error comes from NGINX, give our own error message
-        throw Error('Rate limit exceeded. Try again later.')
-      } else if (response.status != 200) {
-        throw Error(`${response.status} ${await response.text()}`)
-      } else {
-        return response.json()
-      }
-    })
-    .then((data) => {
-      // parse body
-      let output = data as CompilerOutput
-      // check for compiler error
-      if (output.asmOutput == '') {
-        props.setAstXml(output.compilerStdErr + output.compilerStdOut)
-        props.setAssembly(output.compilerStdErr + output.compilerStdOut)
-        // an error occurred, indicate to user and set asm and ast views to combined stderr and stdout streams.
-        throw Error('Compilation error occurred. Check your source program.')
-      } else {
-        // set store variables accordingly
-        props.setAstXml(output.xmlOutput)
-        props.setAssembly(output.asmOutput)
-      }
-    })
-    .catch((error) => {
-      $toast.error(`${error}`)
-    })
+const onPressCompile = () => {
+  props.compile((srcCodeEditor.value as typeof CodeEditor).getCode())
 }
 
 const srcCodeExamples: Record<string, string> = {
