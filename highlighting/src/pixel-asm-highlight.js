19a9d573eaac189ffdf6703045887780d7f57910
MarkMizzi <mizzimark2001@gmail.com>
2024-11-30 21:52:03 +0100

    fix pixel asm highlighting

--- highlighting/src/pixel-asm-highlight.js
+++ highlighting/src/pixel-asm-highlight.js
@@ -4,9 +4,10 @@ export const highlighting = styleTags({
   LineComment: tags.comment,
 
   Opcode: tags.keyword,
-  FunctionLabel: tags.operator,
+  FunctionLabel: tags.name,
 
   Number: tags.number,
   PCOffset: tags.literal,
-  Colour: tags.literal,
+  Colour: tags.string,
+  Label: tags.literal,
 });
