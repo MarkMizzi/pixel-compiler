314c7d7a80ea6dca3c1f346194bf53235199611d
MarkMizzi <mizzimark2001@gmail.com>
2024-12-01 13:14:47 +0100

    add github link and pixel logo

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -6,11 +6,23 @@ import { RouterLink, RouterView } from 'vue-router'
   <header
     class="flex flex-wrap md:justify-start md:flex-nowrap sticky top-0 z-50 w-full justify-self-start"
   >
-    <nav
-      class="absolute z-20 top-0 left-0 w-screen bg-slate-900 px-4 py-2 flex flex-row justify-end gap-x-4"
-    >
-      <RouterLink class="link-green" to="/">home</RouterLink>
-      <RouterLink class="link-green" to="/about">about</RouterLink>
+    <nav class="absolute z-20 top-0 left-0 w-screen bg-slate-900 px-4 py-2 flex flex-row">
+      <div class="flex flex-row justify-start">
+        <img alt="Pixel logo" class="logo" width="30" height="30" src="@/assets/logo.svg" />
+      </div>
+      <div class="flex flex-row justify-end gap-x-4 w-full">
+        <RouterLink class="link-green" to="/">home</RouterLink>
+        <RouterLink class="link-green" to="/about">about</RouterLink>
+        <a href="https://github.com/MarkMizzi/pixel-compiler">
+          <img
+            alt="View source on Github"
+            class="logo"
+            width="30"
+            height="30"
+            src="@/assets/github-mark-white.svg"
+          />
+        </a>
+      </div>
     </nav>
   </header>
 
